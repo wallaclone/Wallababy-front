8da7a4c146be3d5cd9c811e4c98a63c7ba287adc
gemasegarra <gema.segb@gmail.com>
2020-06-25 13:45:33 +0200

    create register api call

--- src/Components/api/api.js
+++ src/Components/api/api.js
@@ -1,1 +1,32 @@
 // api calls with fetch
+
+const apiCall = (API = 'http://localhost:3000/api') => {
+  
+  const registerEndPoint = `${API}/users`
+  const loginEndPoint = `${API}/login`
+  return {
+
+    register: async (username, password, email) => {
+      try {
+        const response = await fetch(registerEndPoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({  
+          'username': username,
+          'password': password,
+          'email': email }),
+        })
+       
+        const data = await response.json();
+        return data;
+      } catch (err) {
+        console.error(err.message);
+        throw err;
+      }
+    }
+  }
+};
+
+export default apiCall;
