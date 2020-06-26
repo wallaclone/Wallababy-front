249c7c5ad506e475c28eab9efc07ff8f94b0a7d1
Sergio Pérez <sergy.perez@gmail.com>
2020-06-26 16:10:25 +0200

    To add PasswordRecovery and Routing to the app, I have done the following: - I create the file: PasswordRecovery.jsx - I create the files: reset.css and variables.css - I add the images: login.png and password.png - I modify the files: App.js and index.js.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import PasswordRecovery from './components/PasswordRecovery.jsx';
 
 function App() {
   return (
-    <div className="App">
-      Hola
-    </div>
+    <Router>
+      <Switch>
+        {/* 
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route exact path="/editAd/id=:_id" component={EditAd} />
+        <Route exact path="/dashboard/:_id" component={Detail} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/createAd" component={CreateAd} /> 
+        */}
+        
+        <Route exact path="/passwordRecovery" component={PasswordRecovery} />
+        <Redirect to="/passwordRecovery" />
+      </Switch>
+    </Router>
   );
 }
 
