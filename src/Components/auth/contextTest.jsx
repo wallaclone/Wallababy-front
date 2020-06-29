355f6afa050873645b336851ec388fcd7c25989b
gemasegarra <gema.segb@gmail.com>
2020-06-29 16:49:23 +0200

    add sign out function and link to navbar

--- src/Components/auth/contextTest.jsx
+++ src/Components/auth/contextTest.jsx
@@ -2,28 +2,39 @@
 // ! don't merge with develop
 
 import React, { useContext } from 'react';
-import { Navbar, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 import { AuthContext } from '../../contexts/authContext';
 
 
 export default function ContextTest(props) {
   const { user, setUser } = useContext(AuthContext);
+  
+  async function logout() {
+    window.localStorage.removeItem('token')
+    setUser(null)
+
+  }
+  const handleClick =  () => {
+      logout()
+    }
+  
+  console.log(`logged user is ${user}`)
 
   return (
     <>
 <Navbar className='navbar'>
-  <Navbar.Brand href="#home">Wallaclone</Navbar.Brand>
+  <Navbar.Brand href="/">Wallaclone Test 1</Navbar.Brand>
   <Navbar.Toggle />
   <Navbar.Collapse className="justify-content-end">
     <Navbar.Text>
-      Signed in as: {user}
+     signed in as {user}  <Link to='/login' onClick={handleClick}>Sign out</Link>
     </Navbar.Text>
    
   </Navbar.Collapse>
-</Navbar> 
- <Button>
-      Logged out
-    </Button>
+</Navbar>
+<div> Hello {user} </div> 
+
     </>
   )
 }
