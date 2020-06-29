8c2f308bd776294322a8aca19b52919f1ea05ef3
gemasegarra <gema.segb@gmail.com>
2020-06-29 08:42:42 +0200

    create current user api call

--- src/Components/api/api.js
+++ src/Components/api/api.js
@@ -1,9 +1,10 @@
 import Swal from 'sweetalert2';
 
 const apiCall = (API = 'http://localhost:3000/api') => {
-  
-  const registerEndPoint = `${API}/users`
-  const loginEndPoint = `${API}/login`
+
+  const registerEndPoint = `${API}/users`;
+  const loginEndPoint = `${API}/login`;
+
   return {
 
     register: async (username, password, email) => {
@@ -13,30 +14,30 @@ const apiCall = (API = 'http://localhost:3000/api') => {
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({  
-          'username': username,
-          'password': password,
-          'email': email }),
+          body: JSON.stringify({
+            'username': username,
+            'password': password,
+            'email': email
+          }),
         })
-        
         if (response.status !== 201) {
           Swal.fire({
-              icon: 'error',
-              title: 'Username or email already in use',
-              timer: 8000,
-              confirmButtonColor:  '#E29578'
+            icon: 'error',
+            title: 'Username or email already in use',
+            timer: 8000,
+            confirmButtonColor: '#E29578'
           });
-      } else {
+        } else {
           Swal.fire({
-              icon: 'success',
-              title: `Welcome ${username}!`,
-              text: ``,
-              timer: 15000,
-              confirmButtonColor:  '#E29578'
+            icon: 'success',
+            title: `Welcome ${username}!`,
+            text: ``,
+            timer: 15000,
+            confirmButtonColor: '#E29578'
           })
-      };
-       
-       
+        };
+
+
         const data = await response.json();
         return data;
 
@@ -46,48 +47,64 @@ const apiCall = (API = 'http://localhost:3000/api') => {
       }
     },
 
-      login: async (username, password) => {
+    login: async (username, password) => {
       try {
         const response = await fetch(loginEndPoint, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({  
-          'username': username,
-          'password': password,
+          body: JSON.stringify({
+            'username': username,
+            'password': password,
           }),
         })
-        
+
         if (response.status !== 201) {
           Swal.fire({
-              icon: 'error',
-              title: 'Ops. Wrong credentials',
-              timer: 8000,
-              confirmButtonColor:  '#E29578'
+            icon: 'error',
+            title: 'Ops. Wrong credentials',
+            timer: 8000,
+            confirmButtonColor: '#E29578'
           });
-      } else {
+        } else {
           Swal.fire({
-              icon: 'success',
-              title: `Welcome ${username}!`,
-              text: ``,
-              timer: 15000,
-              confirmButtonColor:  '#E29578',
+            icon: 'success',
+            title: `Welcome ${username}!`,
+            text: ``,
+            timer: 15000,
+            confirmButtonColor: '#E29578',
           });
-      }
+        }
 
-  
         const data = await response.json();
         window.localStorage.setItem('token', data.token);
-         return data;
+        return data;
+
 
       } catch (err) {
         console.error(err.message);
         throw err;
       }
+    },
+
+    currentUser: async () => {  
+      const loggedUserEndPoint = `${API}/currentuser?token=${window.localStorage.getItem('token')}`;
+      const response = await fetch(loggedUserEndPoint, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      if (!response.ok) {
+        throw new Error('Error ')
+      }
+      const data = await response.json();
+      console.log(data.user)
+      return data.user
     }
   }
 };
-  
+
 
 export default apiCall;
