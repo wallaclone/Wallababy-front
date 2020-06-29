c1c31d4216c99bf8a4b902d711bc58707c92d543
gemasegarra <gema.segb@gmail.com>
2020-06-29 08:40:54 +0200

    create auth context

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,9 +1,37 @@
-// todo 
+// auth context --> sets user to logged user username
 
-import { createContext, useContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
-export const AuthContext = createContext(null);
+import apiCall from '../../src/components/api/api';
 
-export function useAuth(){
-  return useContext(AuthContext)
-}
+const { currentUser } = apiCall();
+
+
+export const AuthContext = createContext(); // user context
+
+const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const user = await currentUser();
+        setUser(user);
+      } catch (error) {
+        setUser(null);
+        console.error(error.message);
+      } 
+    })();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser}}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
+
+// Remove token when user logouts
+/* async function logout() {
+  window.localStorage.removeItem('token')
+} */
