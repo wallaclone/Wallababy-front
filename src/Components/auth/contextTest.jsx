433ed237b146612515ad0436f6818849370dfeb9
gemasegarra <gema.segb@gmail.com>
2020-06-29 08:43:00 +0200

    navbar context test

--- src/Components/auth/contextTest.jsx
+++ src/Components/auth/contextTest.jsx
@@ -1,15 +1,29 @@
-// testing useContext
+// testing useContext - navbar with logged user
+// ! don't merge with develop
 
 import React, { useContext } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
 import { AuthContext } from '../../contexts/authContext';
 
-export default function ContextTest() {
-  const test = useContext(AuthContext) 
+
+export default function ContextTest(props) {
+  const { user, setUser } = useContext(AuthContext);
+
   return (
     <>
-    <h2>Test</h2>
-    <div>Context says:</div>
-    <div>{test}</div>
+<Navbar className='navbar'>
+  <Navbar.Brand href="#home">Wallaclone</Navbar.Brand>
+  <Navbar.Toggle />
+  <Navbar.Collapse className="justify-content-end">
+    <Navbar.Text>
+      Signed in as: {user}
+    </Navbar.Text>
+   
+  </Navbar.Collapse>
+</Navbar> 
+ <Button>
+      Logged out
+    </Button>
     </>
   )
 }
