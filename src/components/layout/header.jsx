af63fe216538da8684d9475f2899aed9d3132a97
gemasegarra <gema.segb@gmail.com>
2020-07-15 21:50:57 +0200

    change setuser

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -12,7 +12,7 @@ function Header(props) {
 
   async function logout() {
     window.localStorage.removeItem('token');
-    setUser(null);
+    setUser('guest');
   }
 
   const handleClick = () => {
